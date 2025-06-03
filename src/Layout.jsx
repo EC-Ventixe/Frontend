e3640bb492151d8948dca3ae5fbec1bed81b503e
ugudmundsson <u.gudmundsson@gmail.com>
2025-06-03 12:00:45 +0200

    hotfix4

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,4 @@
 import { BrowserRouter, Outlet, Route, Router, Routes } from "react-router-dom";
-import "./App.css";
-import "./assets/components/Navbar/navbar.css";
-import "./assets/components/Topbar/topbar.css";
-import "./assets/components/Footer/footer.css";
-import "./assets/pages/Events/events.css";
-import "./assets/components/addEvent.css";
-import "./assets/pages/Dashboard/dashboard.css";
-import Events from "./assets/pages/Events/Events";
-import Bookings from "./assets/pages/Bookings/Bookings";
-import Dashboard from "./assets/pages/Dashboard/Dashboard";
 import Navbar from "./assets/components/Navbar/Navbar";
 import Topbar from "./assets/components/Topbar/Topbar";
 import Footer from "./assets/components/Footer/Footer";
